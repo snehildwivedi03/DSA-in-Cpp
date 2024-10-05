93894daafadfc32313947e5e16a9feffd28d6e61
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-10-06 02:17:32 +0530

    Calc. using switch

---
+++ Day 1-3/calculator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ char op;
+ cout<<"Enter the operand '+','-','*','/': ";
+ cin>>op;
+ float a,b;
+ cout<<"Enter two numbers: ";
+ cin>>a>>b;
+if(b==0 && op=='/'){
+  cout<<"Cannot divide by 0";
+  exit(0);
+}
+ switch(op){
+  case'+' :
+  cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+  break;
+
+    case'-' :
+  cout<<a<<" - "<<b<<" = "<<a-b<<endl;
+  break;
+
+    case'*' :
+  cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+  break;
+
+    case'/' :
+  cout<<a<<" / "<<b<<" = "<<a/b<<endl;
+  break;
+
+  default:
+  cout<<"Enter the valid operator";
+  break;
+ }
+
+    return 0;
+}
