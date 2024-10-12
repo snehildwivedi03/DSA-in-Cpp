cd56331cc0b46c3cf6d8694531fb4cb0d1253934
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-10-12 09:24:04 +0530

    Leap Year Program

--- Day 2/leapyear.cpp
+++ Day 2/leapyear.cpp
@@ -1,5 +1,13 @@
+
 #include<iostream>
 using namespace std;
+//function to calculate leap year
+/*
+A leap year is divisible by 4 then it must be divisible by 100 also by 400. If it is 
+divisible by 100 but not 400 then it is not a leap year.
+
+
+*/
 bool isleapyear(int year){
   if(year%4==0){
     if(year%100==0){
